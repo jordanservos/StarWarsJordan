63be10633105afe4b834ff750df382c4c6816a77
Jordan Servos <jservos11@yahoo.com>
2020-03-02 21:31:47 -0700

    working

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -47,6 +47,10 @@ app.get("/", function(req, res) {
 
 app.get("/add", function(req, res) {
     res.sendFile(path.join(__dirname, "add.html"));
+});
+
+app.get("/allchar", function(req, res) {
+    res.sendFile(path.join(__dirname, "allchar.html"));
 });
 
 // Displays all characters
